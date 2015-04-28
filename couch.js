0d643bab8b20daa2bbb282ba24a1306f8ec8c977
tigoe <tom.igoe@gmail.com>
2015-04-28 13:57:28 -0400

    updated couch example in JS

--- couch.js
+++ couch.js
@@ -1,86 +1,90 @@
 var record = {};
 
 function setup() {
-  console.log('starting');
+  // set a clock to update the Date/Time field every second:
   setInterval(clock, 1000);
 
+  // set event listeners for the buttons.
+  // First get the whole button div:
   var buttonDiv = document.getElementById('buttonDiv');
+  // then get all the buttons in a list:
   var buttons = buttonDiv.getElementsByTagName('*');
-
+  // the iterate over the list and add event listeners:
   for (var b=0; b< buttons.length; b++) {
     buttons[b].addEventListener('click', contactServer, false);
   }
 }
 
+// this function updates the date/time field  once a second:
 function clock() {
   var dateField = document.getElementById('date');
   dateField.value = new Date();
 }
 
+// this function contacts the server:
 function contactServer() {
-  var method = event.target.innerHTML;      // get the method from the button clicked
+  // get the method from the button clicked:
+  var method = event.target.innerHTML;
+
+  // get the record _id and name from the UI fields:
   record._id = document.getElementById('uuid').value;
-  var me = document.getElementById('username').value;
-  record.name = me;
+  record._rev = document.getElementById('rev').value;
+  record.name = document.getElementById('username').value;
 
-var url = 'http://demo.ethoinformatics.org:5984/mike_demo/' + record._id
+  // set up the basic HTTP parameters:
+  var params = {
+    url: 'http://demo.ethoinformatics.org:5984/mike_demo/',
+    type: method,
+    success: callback,
+    contentType: 'application/json',
+    dataType: 'json',
+  }
+  // convert the data to a string for sending:
+  data = JSON.stringify(record);
 
-    data = JSON.stringify(record);
-
-    if (method === 'GET') {
-      $.ajax({
-        url: url,
-        type: method,
-        success: callback
-      });
-    }
-
-    if (method === 'POST') {
-      //url += "?rev=" + record._rev;
-      delete record._rev;
-      delete record._id;
-    
-      data = JSON.stringify(record);
-      $.ajax({
-        url: url,
-        type: method,
-        contentType: 'application/json',
-        dataType: 'json',
-        data: data,
-        success: callback
-      });
-    }
-
-  if (method === 'DELETE') {
-    url += "?rev=" + record._rev;
-    $.ajax({
-      url: url,
-      type: method,
-      success: callback
-    });
+  // GET wants /document/recordID and no request body:
+  if (method === 'GET') {
+    params.url += record._id;
   }
 
+  // POST wants /document/ and a request body with record ID and record Rev:
+  if (method === 'POST') {
+    params.data = data;
+  }
+
+  // PUT wants /document/recordID and a request body with just the data
+  // (ID optional in the body):
   if (method === 'PUT') {
-    $.ajax({
-      url: url,
-      type: method,
-      contentType: 'application/json',
-      dataType: 'json',
-      data: data,
-      success: callback
-    });
+    params.url += record._id;
+    params.data = data;
   }
 
-  console.log(data);
+  // DELETE wants /document/recordID?recordRev and no request body:
+  if (method === 'DELETE') {
+    params.url += record._id;
+    params.url += "?rev=" + record._rev;
+  }
 
+  // make the HTTP call:
+  $.ajax(params);
+  console.dir(data);
 }
 
+// this function gets the request response:
 function callback(data) {
-  record = JSON.parse(data);
-
-
+  // save the response as the local record:
+  record = data;
+  // if there's a name, put it in the name field;
+  if (record.name) {
     document.getElementById('username').value = record.name;
-
+  }
+  // if there's rev or a _rev, put it in the rev field:
+  if (record.rev) {
+    document.getElementById('rev').value = record.rev;
+  }
+  if (record._rev) {
+    document.getElementById('rev').value = record._rev;
+  }
 
   console.dir(record);
 }
