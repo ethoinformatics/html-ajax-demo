983ffc24df7c1abba710247504b396038d4e87d1
tigoe <tom.igoe@gmail.com>
2015-05-07 00:08:59 -0400

    Updates to index.html

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var database = null;    // the current diary document from the DB
 var records = [];      // record ids of the current database
 var currentRecord = 0;    // record you're currently looking at
 
-var foo;
 function setup() {
   // get all the UI buttons:
   previousButton = document.getElementById('previous');
@@ -29,7 +28,6 @@ function getRecords() {
 }
 
 function saveRecords(data) {
-//  database = data;        // save the results locally
   var recordCount = data.rows.length;
   for (var i=0; i<recordCount; i++) {
     var thisId = data.rows[i].id;
@@ -62,15 +60,14 @@ function getRecord(recordNum) {
 }
 
 function display(data) {
-  //console.log(data.footprint);
-   var geojsonFeature = data.footprint;
-
-// TODO: Fix tony's GeoJSON to get it to work.
-if (typeof geojsonFeature === 'object' ) {
-  console.log(geojsonFeature);
-  L.geoJson(geojsonFeature).addTo(map);
-}
-
+//   //console.log(data.footprint);
+//    var geojsonFeature = data.footprint;
+//
+// // TODO: Fix tony's GeoJSON to get it to work.
+// if (typeof geojsonFeature === 'object' ) {
+//   console.log(geojsonFeature);
+//   L.geoJson(geojsonFeature).addTo(map);
+// }
 
   // get the entry element:
   var thisRecord = document.getElementById('entry');
@@ -82,7 +79,7 @@ if (typeof geojsonFeature === 'object' ) {
   for (c = 0; c < children.length; c++) {
     thisChild = children[c];                  // get the current child
     datum = thisChild.id;                     // the id is the db datum name
-  //  thisChild.value = data[datum];       // fill in the data from the db
+    thisChild.value = data[datum];       // fill in the data from the db
   }
 
 }
