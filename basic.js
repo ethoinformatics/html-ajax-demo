9990c999d3505a3fd7c585847860fddbbd139182
tigoe <tom.igoe@gmail.com>
2015-05-09 09:25:55 -0500

    additional comments for basic.js

--- basic.js
+++ basic.js
@@ -3,8 +3,14 @@
   demonstrate simple user interaction
 */
 
-var buttonCount = 0;
+var buttonCount = 0;    // a variable to count the button presses
 
+/*
+  this function is run when the HTML page loads.
+  In basic.html, you'll see a script tag in the body
+  that loads this script.
+  See the bottom of this script, where it's called.
+*/
 function setup() {
   // get the elements:
   var myButton = document.getElementById('pressMe');
@@ -13,10 +19,9 @@ function setup() {
   // add event listeners to listen for user interaction:
   myButton.addEventListener('click', buttonPress, false);
     myField.addEventListener('textInput', echo, false);
-
-
 }
 
+// this is the event listener for the button clicks:
 function buttonPress() {
   // get the element called 'message':
   var messageDiv = document.getElementById('message');
@@ -26,6 +31,7 @@ function buttonPress() {
   buttonCount++;
 }
 
+// this si the event listener for the text field:
 function echo() {
   // get the field in which there was text input:
   var whichField = event.target;
